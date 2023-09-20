333f466b2dd66bd46cd559f43f1206d6c7edca06
Taher-P <145263691+Taher-P@users.noreply.github.com>
2023-09-20 21:14:09 +0530

    Update upgrade_pip.sh

--- upgrade_pip.sh
+++ upgrade_pip.sh
@@ -3,6 +3,13 @@
 # Upgrade pip to the latest version
 pip install --upgrade pip
 
+# Install Python 3.11 using apt-get (for Ubuntu/Debian-based systems)
+sudo apt-get update
+sudo apt-get install python3.11
+
+# Check if Python 3.11 is available
+python3.11 --version
+
 # Make the upgrade_pip.sh script executable
 chmod +x upgrade_pip.sh
 
