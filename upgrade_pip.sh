aa3f49b010fd2f1116ed7ea9acec68eafa1f3e04
Taher-P <145263691+Taher-P@users.noreply.github.com>
2023-09-20 20:43:26 +0530

    Update upgrade_pip.sh

--- upgrade_pip.sh
+++ upgrade_pip.sh
@@ -1,2 +1,15 @@
+# Update pip
 pip install --upgrade pip
+
+# Install Python 3.11 using pyenv
+pyenv install 3.11.0
+
+# Set Python 3.11 as the global version
+pyenv global 3.11.0
+
+# Make sure pip is using the correct Python version
+pip install --upgrade pip
+
+# Make the upgrade_pip.sh script executable
 chmod +x upgrade_pip.sh
+
